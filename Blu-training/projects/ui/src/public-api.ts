688cf429540cf9f4b7a473a0085dcc0e045b4338
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2024-03-28 14:58:41 +0300

    export table config interface

--- Blu-training/projects/ui/src/public-api.ts
+++ Blu-training/projects/ui/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/ui.service';
 export * from './lib/ui.component';
 export * from './lib/table/table.component';
 export * from './lib/table/models/table-data';
+export * from './lib/table/models/table-config';
