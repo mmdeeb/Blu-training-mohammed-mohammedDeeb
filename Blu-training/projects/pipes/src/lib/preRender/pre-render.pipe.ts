decdd895378876a8a4f30f7d64742ee39abdcd78
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2024-04-04 02:20:41 +0300

    update render pipe to accepte the three option

--- Blu-training/projects/pipes/src/lib/preRender/pre-render.pipe.ts
+++ Blu-training/projects/pipes/src/lib/preRender/pre-render.pipe.ts
@@ -6,8 +6,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PreRenderPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(text: string, option: string): string {
 
+    switch (option.toLowerCase()) {
+      case 'uppercase':
+        return text.toUpperCase();
+      case 'lowercase':
+        return text.toLowerCase();
+      case 'camelcase':
+        return this.toCamelCase(text);
+      default:
+        return text; 
+    }
+
+
+  }
+   toCamelCase(text: string): string {
+    return text.split(' ').map((word, index) => {
+      if (index === 0) {
+        return word.toLowerCase();
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }).join('');  }
 }
--- Blu-training/projects/pipes/src/public-api.ts
+++ Blu-training/projects/pipes/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/pipes.service';
 export * from './lib/pipes.component';
 export * from './lib/filter/filter.pipe';
+export * from './lib/preRender/pre-render.pipe';
+
+
