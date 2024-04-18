267c6f6bdc623ccc67bbccd4282f8092d0c82df9
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2024-04-18 18:25:29 +0300

    pass column name to app component and add dialog to send email {the email don't send just print it in the console} when you click on name show dialog has name and welcome

--- Blu-training/src/app/app.component.ts
+++ Blu-training/src/app/app.component.ts
@@ -2,18 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableComponent, TableConfig } from "ui";
 import { DataToTableService } from './services/data-to-table.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'blu-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, TableComponent]
+    imports: [ CommonModule,FormsModule,RouterOutlet, TableComponent]
 })
 export class AppComponent {
 
   public tableData: TableConfig = { columns: [], rows: [] };
   title = 'Blu-training';
+  toastService: any;
+  displayEmailModal: boolean = false;
+  displayWelcomeModal:boolean = false;
+  emailToBeSentTo: string | undefined;
+  emailContent: string | undefined;
+  name: string |undefined;
 
   constructor(private dataToTableService: DataToTableService) {
     this.getTableData();
@@ -23,6 +31,47 @@ export class AppComponent {
     this.dataToTableService.getTableData().subscribe(data => {
       this.tableData = data;
     });
+  }
+
+  onCellClick(value: string, column: string) {
+    switch (column) {
+      case 'Email':
+        this.openEmailCard(value);
+        break;
+      case 'Firstname':
+      case 'Lastname':
+        this.welcomeUser(value);
+        break;
+      default:
+        console.log('Column not recognized');
+    }
+  }
+
+  openEmailCard(email: string) {
+    this.displayEmailModal = true;
+    this.emailToBeSentTo = email;
+    this.emailContent = '';
+  }
+
+  sendEmail() {
+    console.log('Email to:', this.emailToBeSentTo);
+    console.log('Email content:', this.emailContent);
+    this.closeEmailModal();
+  }
+  closeEmailModal() {
+    this.displayEmailModal = false;
+    this.emailToBeSentTo = undefined;
+    this.emailContent = undefined;
+  }
+
+  
+  closeWelcomeModal() {
+    this.displayWelcomeModal = false;
+  }
+  private welcomeUser(name: string) {
+    this.displayWelcomeModal = true;
+    this.name = name;
+
   }
 
 }
